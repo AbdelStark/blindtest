ab231f03e2f01e02aec4033e99da9a6c2173c45a
Abdelhamid Bakhta <abdelhamid.bakhta@consensys.net>
2021-01-08 11:15:57 +0100

    Update BlindTestGame.sol

--- contracts/BlindTestGame.sol
+++ contracts/BlindTestGame.sol
@@ -4,10 +4,14 @@ pragma solidity >=0.4.22 <0.7.0;
 
 contract BlindTestGame {
 
+    address private _owner;
     string private _name;
     string private _originalMusicFileIntegrityHash;
     string private _originalMusicFileURLIntegrityHash;
     uint8 private _maxParticipants;
+    uint8 private _participantsCount;
+    bool private _isOpen;
+    mapping (address => string) private _participants;
 
     constructor(
         string memory name_,
@@ -15,10 +19,13 @@ contract BlindTestGame {
         string memory originalMusicFileURLIntegrityHash_,
         uint8 maxParticipants_
     ) public {
+        _owner = msg.sender;
       _name = name_;
       _originalMusicFileIntegrityHash = originalMusicFileIntegrityHash_;
       _originalMusicFileURLIntegrityHash = originalMusicFileURLIntegrityHash_;
       _maxParticipants = maxParticipants_;
+      _participantsCount = 0;
+      _isOpen = true;
     }
 
     function name() public view returns (string memory) {
@@ -35,5 +42,27 @@ contract BlindTestGame {
 
     function maxParticipants() public view returns (uint8) {
       return _maxParticipants;
+    }
+
+    function isOpen() public view returns (bool) {
+      return _isOpen;
+    }
+
+    modifier onlyWhenOpen() {
+      require(_isOpen, "Game is closed.");
+      _;
+    }
+
+    modifier onlyOwner() {
+      require(_isOpen, "Only owner can call this function.");
+      _;
+    }
+
+    function participate() public onlyWhenOpen{
+        _participants[msg.sender] = "";
+    }
+
+    function close() public onlyOwner{
+        _isOpen = false;
     }
 }
